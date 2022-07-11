5d8acb0c48df4b2187af6447e63398eb4c57997d
Harsh Jani <harshjani6373@gmail.com>
2022-07-11 13:44:25 +0530

    completed project

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner sc = new Scanner(System.in);
+
+        int myNumber =  (int) (Math.random()*100);
+
+        int userNumber = 0;
+
+        do {
+            System.out.println("Guess The Number (1-100) : ");
+            userNumber = sc.nextInt();
+
+            if (userNumber == myNumber){
+                System.out.println("#### WOOHOO You GUESSED NUMBER !!!");
+                break;
+            } else if (userNumber > myNumber) {
+                System.out.println("Your number is large...");
+            }
+            else {
+                System.out.println("Your number is small like your....");
+            }
+        } while (userNumber >= 0);
+
+        System.out.println(" number was :");
+        System.out.println(myNumber);
     }
 }
